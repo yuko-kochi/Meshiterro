1eee119ce45acd7a501e60e58028e60377598e81
happy.k165131@gmail.com <ec2-user@ip-172-31-51-98.ec2.internal>
2021-06-13 11:48:52 +0000

    [Finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '9cdcb0538db5f8ec20d575588597b23dc927c84235265d267d87f7b18c08dd8c09e6c2fb7e130f4ee3cd76a835ace33b84bd63fec44ed0c7bc6ae6557a253650'
+
